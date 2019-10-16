abffd3ab72e06fc0e5a3f5b05ef4d7f207414a55
Lorenzo Chiana <lorenzo.chiana@studio.unibo.it>
2019-10-16 11:48:13 +0200

    Implemented trash scripts

---
+++ PYScripts/horizontal_slicing.py
@@ -0,0 +1,6 @@
+#Horizontal slicing
+a = [[1,2,3],[4,5,6],[7,8,9]]
+#(1) Horizontal rows
+r1 = a[0][:]; r2 = a[1][:]; r3 = a[2][:]
+#(2) For a specific value
+c = a[0][2]; d = a[0][:2]
---
+++ PYScripts/perCasa.py
@@ -0,0 +1,8 @@
+a = 10
+b = int(input("Inserisci un valore: "))
+#inserisi un valore se è maggiore di a motilici per 2
+#altrimenti lo dividi  per 2
+if(b > a):
+    b = b/2
+else:
+    b = b*2
