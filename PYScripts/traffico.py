df5092b26fd3401ad80fdae8c0828fb5b6c42676
Lorenzo Chiana <lorenzo.chiana@studio.unibo.it>
2019-10-23 14:10:20 +0200

    es statistica

--- PYScripts/traffico.py
+++ PYScripts/traffico.py
@@ -10,6 +10,14 @@ npa = df['ago-01'].to_numpy() # numpy array
 #dataframe monodimensionale si chiama series
 #dataframe bidimensionale si chiama frame
 #dataframe tridimensionale si chiama panel
+
+res = stats.relfreq(npa, numbins=10)
+print(res[0])
+
+print('Media: ', np.mean(npa))
+print('Mediana: ', np.median(npa))
+print('Moda: ', stats.mode(npa)[0])
+
 plt.hist(npa, bins=10, color='#00AA00', edgecolor='black') #plt = pyplot; hist = histogram
 plt.title(df.columns[0])
 plt.xlabel('num')
